8f3b7e274f568f075ba83b164609b641daa5e958
Per Thomas Lundal <perthomas@gmail.com>
2014-12-03 16:54:23 +0100

    Update tests

--- 3D/sblocktest.c
+++ 3D/sblocktest.c
@@ -1,16 +1,13 @@
-/* sblocktest.c
+/*****************************************************************************
+ * Tests
  *
- * A testprogram for sblockmatrix project
+ * Various tests to verify the correctness of the platform
  *
- * This runs same test experiments that is described in the master
- * thesis report.  
- *
- * Asbjorn Djupdal 2003
+ * Asbjørn Djupdal 2003
  * Kjetil Aamodt 2005
- * Ola Martin Tiseth Stoevneng 2014
- *
- * Stripped down to just generate a modelsim script file for the tests
- */
+ * Ola Martin Tiseth Støvneng 2014
+ * Per Thomas Lundal 2014
+ *****************************************************************************/
 
 #include "constants.h"
 #include "sblocklib.h"
@@ -25,243 +22,112 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-/*****************************************************************************/
-/* protos */
+/* Protypes */
 
-/* write a specific state pattern to BRAM-0 */
-void setStatesAlternating (void);
+void reset();
+void setStatesAlternating();
+void printAllTypes();
 
-/* write program to co-processor */
-void store_program_test_funct(void);
-void store_program_test_rule(void);
-void store_program_test_speed(void);
-void store_program_test_fitness(void);
-void store_program_test_fitness_speed_new(int runstep);
+void test_write_read_types();
+void test_development();
 
-/*****************************************************************************/
-/* globals */
+/* Globals */
 
-enum test_t {TEST_FUNCT = 0, TEST_SPEED, TEST_RULE, TEST_FITNESS, TEST_FITNESS_SPEED_NEW};
+enum test_t {TEST_WRITE_READ_TYPES, TEST_DEVELOPMENT};
 enum test_t test;
 
-char *modelsim;
-
-/*****************************************************************************/
-/* functions */
+/* Main */
 
 int main (int argc, char* argv[]) {
-  if(argc > 1){
+  if (argc > 1) {
     test = atoi(argv[1]);
-    modelsim = argv[2];
 
-    printf("Test: \t\t%i\nModelsim: \t%s\n\n", test, modelsim);
+    printf("Test: %i\n", test);
     fflush(stdout);
-    
-    /* write to lut conv table */
-    writeLUTConv(EMPTY_LO, EMPTY_HI, 0); /* sblock type 0 is empty */
-    writeLUTConv(XOR4_LO, XOR4_HI, 1);   /* sblock type 1 is XOR4 */
-    writeLUTConv(OR4_LO, OR4_HI, 2);     /* sblock type 2 is OR4 */
-    writeLUTConv(AND4_LO, AND4_HI, 3);   /* sblock type 3 is AND4 */
 
-    /* write rules to rule storage */
-    writeRule(create_rule_grow_south_1(), 0);
-    writeRule(create_rule_grow_south_2(), 1);
-    writeRule(create_rule_grow_east_1(), 2);
-    writeRule(create_rule_grow_east_2(), 3);
-    writeRule(create_rule_grow_north_1(), 4);
-    writeRule(create_rule_grow_north_2(), 5);
-    writeRule(create_rule_grow_west_1(), 6);
-    writeRule(create_rule_grow_west_2(), 7);
-    writeRule(create_rule_change_a_1(), 8);
-    writeRule(create_rule_change_a_2(), 9);
-    writeRule(create_rule_change_b_1(), 10);
-    writeRule(create_rule_change_b_2(), 11);
+    openCard();
 
-    setNumberOfLastRule  (11);
-    resetDevCounter();
-    clearBRAM(0, 0);
+    reset();
 
-    switch (test){
-    case TEST_FUNCT:
-      printf("Test Function\n");
-      store_program_test_funct();
-      saveSendBuffer (modelsim);
+    switch (test) {
+    case TEST_WRITE_READ_TYPES:
+      printf("Test Write Read Types\n");
+      test_write_read_types();
       break;
-    case TEST_SPEED:
-      printf("Test Speed\n");
-      store_program_test_speed();
-      saveSendBuffer (modelsim);
-      break;
-    case TEST_RULE:
-      printf("Test Rule\n");
-      store_program_test_rule();
-      saveSendBuffer (modelsim);
-      break;
-    case TEST_FITNESS:
-      printf("Test Fitness\n");
-      store_program_test_fitness();
-      saveSendBuffer(modelsim);
-      break;
-    case TEST_FITNESS_SPEED_NEW:
-      printf("Test Fitness speed new\n");
-      store_program_test_fitness_speed_new(1000);
-      saveSendBuffer(modelsim);
+    case TEST_DEVELOPMENT:
+      printf("Test Development\n");
+      test_development();
       break;
     default:
       break;
     }
 
-  } else fprintf (stderr, "Arguments: <test> <modelsim scriptfile>\n");
+    closeCard();
+  }
+  else {
+    fprintf(stderr, "Arguments: <test>\n");
+  }
   return 0;
 }
 
-void setStatesAlternating (void) {
-  int i, j, k;
-  for (k = 0; k < (LAYERS / 2); k++) {
-    for (i = 0; i < (ROWS / 2); i++) {
-      for (j = 0; j < (COLUMNS / 16); j++) {
-        writeStates (0x5555555555555555, j * 16, i * 2, k * 2);
+/* Utility */
+
+void reset() {
+  clearBRAM(0, 0);
+  resetDevCounter();
+  flushDMA();
+}
+
+void setStatesAlternating() {
+  for (int z = 0; z < (LAYERS / 2); z++) {
+    for (int y = 0; y < (ROWS / 2); y++) {
+      for (int x = 0; x < (COLUMNS / 16); x++) {
+        writeStates(0x5555555555555555, x * 16, y * 2, z * 2);
       }
     }
   }
 }
 
-void store_program_test_rule(){
-  switchSBMs();
-
-  store(PROGRAM_ADDRESS);{
-    config();
-    readback();
-    switchSBMs();
-    readTypes();
-    devstep();
-    readUsedRules();
-    
-    jumpEqual(20, PROGRAM_ADDRESS + 30);
-
-    jump(PROGRAM_ADDRESS);
-  }end();
-
-  store(PROGRAM_ADDRESS + 30);{
-    readRuleVector(20);
-    jump(PROGRAM_ADDRESS);
-  }end();
-  jump(PROGRAM_ADDRESS);
+void printAllTypes() {
+  for (int z = 0; z < LAYERS; z++) {
+    printf("Layer %d:\n", z);
+    readDMATypes();
+    printTypes(stdout);
+  }
 }
 
-void store_program_test_funct(){
-  /* set states */
-  setStatesAlternating();
-  
-  /* set types */
-  writeType (2, 4, 1, 1);
+/* Tests */
 
-  /* change so that initial data is in BRAM-1 */
-  switchSBMs();
-  
-  /* store program */
-  store (PROGRAM_ADDRESS); {
-    config();
-    run (77);
-    readback();
-    
-    switchSBMs();
+void test_write_read_types() {
+  writeType(0, 0,0,0);
+  writeType(1, 1,1,0);
+  writeType(2, 2,2,0);
+  writeType(3, 3,3,0);
 
-    devstep();
-    jumpEqual(10, PROGRAM_ADDRESS + 30);
-    jump (PROGRAM_ADDRESS);
-  }end();
-  store (PROGRAM_ADDRESS + 30); {
-    readTypes();
-    readStates();
-  }end();
-  jump(PROGRAM_ADDRESS);
+  readTypes();
+
+  flushDMA();
+
+  printAllTypes();
 }
 
-void store_program_test_speed(){
-  /* set states */
-  setStatesAlternating();
-  
-  /* set types */
-  writeType (2, 4, 1, 0);
+void test_development() {
+  writeRule(create_rule_1_to_2(), 0);
+  writeRule(create_rule_2_to_3(), 1);
+  setNumberOfLastRule(1);
 
-  /* change so that initial data is in BRAM-1 */
+  writeType(0, 0,0,0);
+  writeType(1, 1,1,0);
+  writeType(2, 2,2,0);
+  writeType(3, 3,3,0);
+
+  devstep();
   switchSBMs();
-  
-  /* store program */
-  store (PROGRAM_ADDRESS); {
-    config();
-    run (50000);
-    readback();
-    
-    switchSBMs();
 
-    readTypes();
-    readStates();
+  readTypes();
 
-    devstep();
+  flushDMA();
 
-    jump (PROGRAM_ADDRESS);
-  }end();
-  jump(PROGRAM_ADDRESS);
-}
-
-void store_program_test_fitness(void){
-  /* set states */
-  setStatesAlternating();
-  /* set types */
-  writeType (2, 4, 1, 0);
-  /* change so that initial data is in BRAM-1 */
-  switchSBMs();
-  
-  /* store program */
-  store (PROGRAM_ADDRESS); {
-    config();
-    run (100);
-    readback();
-    
-    switchSBMs();
-
-    readStates();
-    readSums(100);
-
-    doFitness(100);
-    readFitness();
-    readFitness();
-    readFitness();
-    readFitness();
-    devstep();
-
-    jump (PROGRAM_ADDRESS);
-  }end();
-  jump(PROGRAM_ADDRESS);
-}
-
-void store_program_test_fitness_speed_new(int runstep){
-  /* set states */
-  setStatesAlternating();
-  /* set types */
-  writeType (2, 4, 1, 0);
-  /* change so that initial data is in BRAM-1 */
-  switchSBMs();
-  
-  /* store program */
-  store (PROGRAM_ADDRESS); {
-    config();
-    run (runstep);
-    readback();
-    
-    switchSBMs();
-
-    doFitness(runstep);
-    readFitness();
-    readFitness();
-    readFitness();
-    readFitness();
-    devstep();
-
-    jump (PROGRAM_ADDRESS);
-  }end();
-  jump(PROGRAM_ADDRESS);
+  printAllTypes();
 }
 
