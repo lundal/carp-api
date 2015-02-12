bfa75d177015300e6d2c37ddbd13970aaffa85d2
Per Thomas Lundal <perthomas@gmail.com>
2015-02-12 01:27:37 +0100

    Fix bitvector insertion

--- bitvector.c
+++ bitvector.c
@@ -16,15 +16,29 @@
 
 bitvector_t bitvector_create(uint32_t number_of_bits) {
   uint32_t number_of_parts = div_ceil(number_of_bits, 32);
-  uint32_t *vector_parts = malloc(number_of_parts * sizeof(uint32_t));
+  uint32_t *vector_parts = calloc(number_of_parts, sizeof(uint32_t));
   return (bitvector_t){vector_parts, number_of_parts, 0};
 }
 
 void bitvector_add(bitvector_t *vector, uint32_t data, uint32_t bits) {
-  for (int i = 0; i < vector->number_of_parts; i++) {
-    int start_bit = vector->number_of_bits;
-    int shift_amount = start_bit - i*32;
-    vector->vector_parts[i] |= (data << shift_amount);
+  int first_bit = vector->number_of_bits;
+  int last_bit = first_bit + bits - 1;
+  int first_word = first_bit / 32;
+  int last_word = last_bit / 32;
+
+  /* Bounds check */
+  if (last_word > vector->number_of_parts - 1) {
+    last_word = vector->number_of_parts - 1;
+  }
+
+  for (int i = first_word; i <= last_word; i++) {
+    int shift_amount = vector->number_of_bits - i*32;
+    if (shift_amount > 0) {
+      vector->vector_parts[i] |= (data << shift_amount);
+    }
+    else {
+      vector->vector_parts[i] |= (data >> (-shift_amount));
+    }
   }
   vector->number_of_bits += bits;
 }
